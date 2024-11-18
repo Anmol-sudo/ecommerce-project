8340f6a30423313f3677647261b057b1e5c93076
Anmol Sharma <anmolshiv1974@gmail.com>
2024-11-18 04:30:13 +0000

    feat: Add Data Table(Admin) component and display API routes on frontend
    - Implement Data Table component for admin interface
    - Create API route display functionality in the frontend
    - Update middleware to allow public access to API routes
    - Enhance user experience with interactive data management

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,7 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 
 
 export default async function DashboardLayout({
